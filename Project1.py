2e6f211d380d9dd3610ecf8c3e71bf3e13cf444b
Jana Pohlidalova <pohlidalova.jana@gmail.com>
2025-06-19 14:10:09 +0200

    New file added to GitHub

--- Project1.py
+++ Project1.py
@@ -1,3 +1,5 @@
+import decimal
+
 print("This is my project in Python, watch me closely for more updates")
 print("New line of code added")
 print("This line will be added to staging area using git add . ")
